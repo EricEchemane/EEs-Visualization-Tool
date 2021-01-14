80df8448f819517fa6c5637a2ae0191dcc513b80
Eric Echemane <eechemane29@gmail.com>
2021-01-14 22:21:51 +0800

    binary search is working and animation frames are working but not yet implemented

--- src/components/visualizers/searching/searchingAlgorithms.tsx
+++ src/components/visualizers/searching/searchingAlgorithms.tsx
@@ -11,12 +11,13 @@ interface Frame {
 let LINEAR_SEARCH_ANIMATIONS = [] as Frame[];
 let BINARY_SEARCH_ANIMATIONS = [] as Frame[];
 
-export default function getAnimations(array: number[], searchItem: number) 
-{
+export default function getAnimations(array: number[], searchItem: number) {
     // call this first...
     resetAnimations();
     fill_LinearAnimationFrames(array, searchItem);
-    console.log(LINEAR_SEARCH_ANIMATIONS)
+    fill_BinaryAnimationFrames(array, searchItem);
+
+    console.log(BINARY_SEARCH_ANIMATIONS)
 
     return [
         LINEAR_SEARCH_ANIMATIONS,
@@ -31,9 +32,9 @@ function resetAnimations() {
 
 // -----> fill linear animation frams         
 function fill_LinearAnimationFrames(array: number[], searchItem: number) {
-    for(let x = 0; x < array.length; x++) {
+    for (let x = 0; x < array.length; x++) {
         let newFrame: Frame;
-        if(array[x] == searchItem) {
+        if (array[x] == searchItem) {
             newFrame = {
                 state: FOUND,
                 lookupIndex: x, // the index where the saerchItem were found
@@ -64,6 +65,78 @@ function fill_LinearAnimationFrames(array: number[], searchItem: number) {
 }
 
 // -----> BINARY SEARCH    
-function fill_BinaryAnimationFrames(array: number, searchItem: number) {
-    
+function fill_BinaryAnimationFrames(array: number[], searchItem: number) {
+    let sortedArray = new Array(array.length);
+    sortedArray = Object.assign([], array);
+    sortedArray = sortedArray.sort((a: number, b: number) => a - b);
+    // console.log(searchItem)
+    // console.log(sortedArray)
+    binSearch(
+        sortedArray,
+        searchItem,
+        0,
+        sortedArray.length - 1,
+        0
+    )
+}
+
+function binSearch(
+    sortedArray: number[],
+    searchItem: number,
+    lowerBound: number,
+    upperBound: number,
+    NoOfSteps: number): number {
+
+    if(lowerBound > upperBound) return -1;
+
+    NoOfSteps += 1;
+    let midIndex = (upperBound + lowerBound + 1) / 2;
+    midIndex = Math.floor(midIndex);
+    let midElement = sortedArray[midIndex];
+
+    let newFrame: Frame;
+
+    if (midElement == searchItem) {
+        newFrame = {
+            state: FOUND,
+            lookupIndex: midIndex,
+            steps: NoOfSteps
+        }
+        BINARY_SEARCH_ANIMATIONS.push(newFrame);
+        // FOR DEBUGGING PURPOSES
+        // alert(`found at index ${midIndex}`)
+        return midIndex;
+    }
+    // if not the middle check the lower bound 
+    // if the searchItem is less than mid Element
+    newFrame = {
+        state: CHANGE_COLOR,
+        lookupIndex: midIndex,
+        steps: NoOfSteps
+    }
+    BINARY_SEARCH_ANIMATIONS.push(newFrame);
+    newFrame = {
+        state: REVERT_COLOR,
+        lookupIndex: midIndex,
+        steps: NoOfSteps
+    }
+    BINARY_SEARCH_ANIMATIONS.push(newFrame);
+
+    if (searchItem < midElement) {
+        return binSearch(
+            sortedArray,
+            searchItem,
+            lowerBound,
+            midIndex - 1,
+            NoOfSteps
+        )
+    }
+    // otherwise
+    return binSearch(
+        sortedArray,
+        searchItem,
+        midIndex + 1,
+        upperBound,
+        NoOfSteps
+    )
 }
