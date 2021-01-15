5526891aee6fccf279d465c3c600050e24c14de2
Eric Echemane <eechemane29@gmail.com>
2021-01-15 11:12:51 +0800

    Path Finding Visualizer tsx file added

---
+++ src/components/visualizers/pathFinding/PathFindingVisualizer.tsx
@@ -0,0 +1,15 @@
+import {Box} from '@material-ui/core';
+
+export default function PathFindingVisualizer() {
+    return (
+        <Box
+            p={4}
+            className="flat searchVisualizerContainer "
+            style={{
+                width: '98vw', height: '80vh', position: 'relative',
+                margin: 'auto', top: '1rem', overflow: 'hidden',
+                borderRadius: '10px'}} >
+            
+        </Box>
+    )
+}
