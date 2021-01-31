30b7e4824ecf3a0dcf95f3b237ddd7fdb50b051b
Eric Echemane <eechemane29@gmail.com>
2021-01-31 14:11:04 +0800

    Path finding algorithms select component added

--- src/components/visualizers/pathFinding/PathFindingVisualizer.tsx
+++ src/components/visualizers/pathFinding/PathFindingVisualizer.tsx
@@ -2,17 +2,39 @@
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import { Box } from '@material-ui/core';
 
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 export default function PathFindingVisualizer()
 {
     const [algoOptionsDropdown, set_algoOptionsDropdown] = useState(false);
+    const [activeAlgo, setActiveAlgo] = useState({id:-1,name:'Choose Algorithm'});
+
+    const algortihms = useRef([
+        {id: 0, name: 'Breadth First Search'},
+        {id: 1, name: 'Depth First Search'},
+        {id: 2, name: 'Dijkstra\'s'},
+        {id: 3, name: 'A*'}
+    ])
+
+    // FUNCTIONS @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
     const ShowAlgoOptionsDropdown = () =>
     {
         set_algoOptionsDropdown((prev: boolean) => !prev);
     }
 
+    const selectAlgo = (id: number) =>
+    {
+        setActiveAlgo(algortihms.current[id]);
+        set_algoOptionsDropdown(false);
+    }
+
+    // OTHER VARIABLES @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    const algoOptions = algortihms.current.map((each: any) => 
+        <div key={each.id} onClick={()=>{selectAlgo(each.id)}} > {each.name} </div>
+    )
+
+    // MARK UP @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
     return (
         <Box
          p={4}
@@ -23,25 +45,24 @@ export default function PathFindingVisualizer()
             borderRadius: '10px'
             }}>
             
-            <div id="pathFinding-panel-container">
-                <div>
-                    <div className={"pathfinding-algoOptions"}>
-                        <div className="pathfinding-algo-dropdown" onClick={ShowAlgoOptionsDropdown}>
-                            <div>Choose Algortihm</div>
-                            <ArrowDropDownIcon />
-                        </div>
-                        
-                        <Box hidden={!algoOptionsDropdown}>
-                            <div className="pathAlgo-options">
-                                <div> Breadth First Search </div>
-                                <div> Depth First Search </div>
-                                <div> Dijkstra's </div>
-                                <div> A* </div>
-                            </div>
-                        </Box>
-                    </div>
+            <div className={"pathfinding-algoOptions"}>
+                <div className="pathfinding-algo-dropdown" onClick={ShowAlgoOptionsDropdown}>
+                    <div> {activeAlgo.name} </div>
+                    <ArrowDropDownIcon />
                 </div>
+                
+                <Box hidden={!algoOptionsDropdown}>
+                    <div className="pathAlgo-options">
+                        {algoOptions}
+                    </div>
+                </Box>
             </div>
+
+            
+            <div className="search-field">
+                Hello
+            </div>
+            
          </Box>
     )
 }
